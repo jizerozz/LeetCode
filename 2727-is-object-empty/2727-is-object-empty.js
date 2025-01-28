e78c4db998d886d7a86acbe263064322ac1f6d07
jizerozz <jizero27@gmail.com>
2025-01-28 14:56:26 +0900

    Time: 33 ms (99.81%), Space: 51.6 MB (5.04%) - LeetHub

---
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -0,0 +1,8 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    console.log(Object.keys(obj).length)
+    return Object.keys(obj).length !== 0 ? false : true
+};
